41a0b59e24db39a1ea54be74abf409bd26088bdd
Danil Petrov <danilpetrovvi@gmail.com>
2020-03-07 09:32:23 +1000

    Produce a separate log message before each EOL.
    This commit changes the behaviour of `logging.writer` to produce a
    separate log message before each end-of-line (EOL) is encountered in the
    string.
    
    The writer also internally buffers any strings not terminated with an EOL
    character until the next call to `Write()` will have string with an EOL character in it.
    Then the entire buffered content before an EOL character is produced into a
    separate log message.

--- logging/writer.go
+++ logging/writer.go
@@ -1,29 +1,70 @@
 package logging
 
-import "io"
+import (
+	"bytes"
+	"io"
+	"strings"
+	"sync"
+)
 
 // writer is an implementation of io.Writer that writes to a logger.
 type writer struct {
 	l Logger
 	f func(Logger, string)
+
+	mu  sync.Mutex
+	buf strings.Builder
 }
 
 // NewWriter returns an io.Writer that writes content to the given logger.
 //
-// Each call to Write() produces a separate log message.
+// The string passed to Write() is sliced by end-of-line (EOL) characters and
+// each substring in between the EOL characters is written as a separate log
+// message.
+//
+// If the entire string or its last section does not terminate with an EOL
+// character, it is buffered inside the writer. The writer keeps buffering the
+// string until it detects an EOL character and produces a log message with the
+// buffered string content before the EOL character.
 func NewWriter(l Logger) io.Writer {
-	return &writer{l, LogString}
+	return &writer{l: l, f: LogString}
 }
 
 // NewDebugWriter returns an io.Writer that writes content to the given logger
 // as debug messages.
 //
-// Each call to Write() produces a separate log message.
+// The string passed to Write() is sliced by end-of-line (EOL) characters and
+// each substring in between the EOL characters is written as a separate log
+// message.
+//
+// If the entire string or its last section does not terminate with an EOL
+// character, it is buffered inside the writer. The writer keeps buffering the
+// string until it detects an EOL character and produces a log message with the
+// buffered string content before the EOL character.
 func NewDebugWriter(l Logger) io.Writer {
-	return &writer{l, DebugString}
+	return &writer{l: l, f: DebugString}
 }
 
 func (w *writer) Write(data []byte) (int, error) {
-	w.f(w.l, string(data))
-	return len(data), nil
+	var n int
+
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	i := bytes.IndexRune(data, '\n')
+	for i != -1 {
+		w.buf.Write(data[:i])
+		w.f(w.l, w.buf.String())
+		n += w.buf.Len()
+		w.buf.Reset()
+
+		data = data[i+1:]
+
+		i = bytes.IndexRune(data, '\n')
+	}
+
+	// Write the remaining data, if any, into the temporary string buffer
+	w.buf.Write(data)
+
+	return n, nil
 }
--- logging/writer_test.go
+++ logging/writer_test.go
@@ -11,14 +11,36 @@ var _ = Describe("NewWriter", func() {
 		logger := &BufferedLogger{}
 		writer := NewWriter(logger)
 
-		m := []byte("<message>")
+		m := []byte("<message1>\n<message2>\n<message3>")
 		n, err := writer.Write(m)
 		Expect(err).ShouldNot(HaveOccurred())
-		Expect(n).To(Equal(len(m)))
+		Expect(n).To(Equal(20))
 
 		Expect(logger.Messages()).To(ConsistOf(
 			BufferedLogMessage{
-				Message: "<message>",
+				Message: "<message1>",
+				IsDebug: false,
+			},
+			BufferedLogMessage{
+				Message: "<message2>",
+				IsDebug: false,
+			},
+		))
+
+		logger.Reset()
+
+		m = []byte("<message4>\n<message5>\n")
+		n, err = writer.Write(m)
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(n).To(Equal(30))
+
+		Expect(logger.Messages()).To(ConsistOf(
+			BufferedLogMessage{
+				Message: "<message3><message4>",
+				IsDebug: false,
+			},
+			BufferedLogMessage{
+				Message: "<message5>",
 				IsDebug: false,
 			},
 		))
@@ -30,14 +52,36 @@ var _ = Describe("NewDebugWriter", func() {
 		logger := &BufferedLogger{CaptureDebug: true}
 		writer := NewDebugWriter(logger)
 
-		m := []byte("<message>")
+		m := []byte("<message1>\n<message2>\n<message3>")
 		n, err := writer.Write(m)
 		Expect(err).ShouldNot(HaveOccurred())
-		Expect(n).To(Equal(len(m)))
+		Expect(n).To(Equal(20))
 
 		Expect(logger.Messages()).To(ConsistOf(
 			BufferedLogMessage{
-				Message: "<message>",
+				Message: "<message1>",
+				IsDebug: true,
+			},
+			BufferedLogMessage{
+				Message: "<message2>",
+				IsDebug: true,
+			},
+		))
+
+		logger.Reset()
+
+		m = []byte("<message4>\n<message5>\n")
+		n, err = writer.Write(m)
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(n).To(Equal(30))
+
+		Expect(logger.Messages()).To(ConsistOf(
+			BufferedLogMessage{
+				Message: "<message3><message4>",
+				IsDebug: true,
+			},
+			BufferedLogMessage{
+				Message: "<message5>",
 				IsDebug: true,
 			},
 		))
