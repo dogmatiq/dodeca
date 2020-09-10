16b87057c2b3b999138263bc1f6f2c80713494f7
James Harris <mailjamesharris@gmail.com>
2020-09-10 12:25:28 +1000

    Replace `GetBool()` with `AsBool()`.

--- config/bool.go
+++ config/bool.go
@@ -5,145 +5,72 @@ import (
 	"strings"
 )
 
-// GetBool returns the boolean representation of the value associated with k.
+// AsBool returns the boolean representation of the value associated with k or
+// panics if unable to do so.
 //
-// If the value is "true", "yes" or "on", v and ok are both true.
+// It returns true if the value "true", "yes" or "on", or false if the value is
+// "false", "no" or "off".
+func AsBool(b Bucket, k string) bool {
+	if v, ok := asBool(b, k); ok {
+		return v
+	}
+
+	panic(fmt.Sprintf("%s is not defined", k))
+}
+
+// AsBoolT returns the boolean representation of the value associated with k,
+// or true if k is undefined.
 //
-// If the value is "false", "no" or "off", v is false and ok is true.
+// It returns true if the value "true", "yes" or "on", or false if the value is
+// "false", "no" or "off".
+func AsBoolT(b Bucket, k string) bool {
+	return AsBoolDefault(b, k, true)
+}
+
+// AsBoolF returns the boolean representation of the value associated with k,
+// or true if k is undefined.
 //
-// If k is undefined, ok is false and err is nil.
+// It returns true if the value "true", "yes" or "on", or false if the value is
+// "false", "no" or "off".
+func AsBoolF(b Bucket, k string) bool {
+	return AsBoolDefault(b, k, false)
+}
+
+// AsBoolDefault returns the boolean representation of the value associated
+// with k, or the default value v if k is undefined.
 //
-// If k is defined but its value is not listed above, err is a non-nil error
-// describing the invalid value.
-func GetBool(b Bucket, k string) (v bool, ok bool, err error) {
+// It returns true if the value "true", "yes" or "on", or false if the value is
+// "false", "no" or "off".
+func AsBoolDefault(b Bucket, k string, v bool) bool {
+	if v, ok := asBool(b, k); ok {
+		return v
+	}
+
+	return v
+}
+
+func asBool(b Bucket, k string) (bool, bool) {
 	x := b.Get(k)
 
 	if x.IsZero() {
-		return false, false, nil
+		return false, false
 	}
 
 	s, err := x.AsString()
 	if err != nil {
-		return false, false, err
+		panic(fmt.Sprintf("cannot read %s: %s", k, err))
 	}
 
 	switch strings.ToLower(s) {
 	case "true", "yes", "on":
-		return true, true, nil
+		return true, true
 	case "false", "no", "off":
-		return false, true, nil
+		return false, true
 	default:
-		return false, false, fmt.Errorf(
-			`%s is not a boolean, got %#v, expected "true", "false", "yes", "no", "on" or "off"`,
+		panic(fmt.Sprintf(
+			`expected %s to be a boolean ("true", "false", "yes", "no", "on" or "off"), got %#v`,
 			k,
 			s,
-		)
+		))
 	}
-}
-
-// GetBoolT returns the boolean representation of the value associated with k,
-// or true if k is undefined.
-//
-// If the value is "true", "yes" or "on", it returns true.
-//
-// If the value is "false", "no" or "off", it returns false.
-//
-// If k is defined but its value is not listed above, it returns an error
-// describing the invalid value.
-func GetBoolT(b Bucket, k string) (bool, error) {
-	return GetBoolDefault(b, k, true)
-}
-
-// GetBoolF returns the boolean representation of the value associated with k,
-// or false if k is undefined.
-//
-// If the value is "true", "yes" or "on", it returns true.
-//
-// If the value is "false", "no" or "off", it returns false.
-//
-// If k is defined but its value is not listed above, it returns an error
-// describing the invalid value.
-func GetBoolF(b Bucket, k string) (bool, error) {
-	return GetBoolDefault(b, k, false)
-}
-
-// GetBoolDefault returns the boolean representation of the value associated
-// with k, or the default value v if k is undefined.
-//
-// If the value is "true", "yes" or "on", it returns true.
-//
-// If the value is "false", "no" or "off", it returns false.
-//
-// If k is defined but its value is not listed above, it returns an error
-// describing the invalid value.
-func GetBoolDefault(b Bucket, k string, v bool) (bool, error) {
-	x, ok, err := GetBool(b, k)
-	if err != nil {
-		return false, err
-	}
-
-	if ok {
-		return x, nil
-	}
-
-	return v, nil
-}
-
-// MustGetBool returns the boolean representation of the value associated with
-// k or panics if unable to do so.
-//
-// If the value is "true", "yes" or "on", v and ok are both true.
-//
-// If the value is "false", "no" or "off", v is false and ok is true.
-//
-// If k is undefined, ok is false.
-//
-// It panics if k is defined but its value is not listed above.
-func MustGetBool(b Bucket, k string) (v bool, ok bool) {
-	v, ok, err := GetBool(b, k)
-	if err != nil {
-		panic(err)
-	}
-
-	return v, ok
-}
-
-// MustGetBoolT returns the boolean representation of the value associated with
-// k, or true if k is undefined.
-//
-// If the value is "true", "yes" or "on", it returns true.
-//
-// If the value is "false", "no" or "off", it returns false.
-//
-// It panics if k is defined but its value is not listed above.
-func MustGetBoolT(b Bucket, k string) bool {
-	return MustGetBoolDefault(b, k, true)
-}
-
-// MustGetBoolF returns the boolean representation of the value associated with
-// k, or false if k is undefined.
-//
-// If the value is "true", "yes" or "on", it returns true.
-//
-// If the value is "false", "no" or "off", it returns false.
-//
-// It panics if k is defined but its value is not listed above.
-func MustGetBoolF(b Bucket, k string) bool {
-	return MustGetBoolDefault(b, k, false)
-}
-
-// MustGetBoolDefault returns the boolean representation of the value associated
-// with k, or the default value v if k is undefined.
-//
-// If the value is "true", "yes" or "on", it returns true.
-//
-// If the value is "false", "no" or "off", it returns false.
-//
-// It panics if k is defined but its value is not listed above.
-func MustGetBoolDefault(b Bucket, k string, v bool) bool {
-	if x, ok := MustGetBool(b, k); ok {
-		return x
-	}
-
-	return v
 }
--- config/bool_test.go
+++ config/bool_test.go
@@ -1,10 +1,6 @@
 package config_test
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/dogmatiq/dodeca/config"
 	. "github.com/dogmatiq/dodeca/config"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
@@ -21,63 +17,42 @@ var boolTableEntries = []TableEntry{
 	Entry("falsey: off", "off", false),
 }
 
-var _ = Describe("func GetBool()", func() {
+var _ = Describe("func AsBool()", func() {
 	DescribeTable(
-		"it returns the expected value",
+		"it returns a boolean value",
 		func(str string, expect bool) {
 			b := Map{"<key>": String(str)}
 
-			v, ok, err := GetBool(b, "<key>")
-			Expect(err).ShouldNot(HaveOccurred())
+			v := AsBool(b, "<key>")
 			Expect(v).To(Equal(expect))
-			Expect(ok).To(BeTrue())
 		},
 		boolTableEntries...,
 	)
 
-	It("sets ok to false if the key is not defined", func() {
+	It("panics if the key is not defined", func() {
 		b := Map{}
 
-		_, ok, err := GetBool(b, "<key>")
-		Expect(err).ShouldNot(HaveOccurred())
-		Expect(ok).To(BeFalse())
+		Expect(func() {
+			AsBool(b, "<key>")
+		}).To(PanicWith(`<key> is not defined`))
 	})
 
-	It("returns an error if the value is not one of the accepted values", func() {
+	It("panics if the value is not one of the accepted values", func() {
 		b := Map{"<key>": String("<invalid>")}
 
-		_, _, err := GetBool(b, "<key>")
-		Expect(err).To(MatchError(`<key> is not a boolean, got "<invalid>", expected "true", "false", "yes", "no", "on" or "off"`))
+		Expect(func() {
+			AsBool(b, "<key>")
+		}).To(PanicWith(`expected <key> to be a boolean ("true", "false", "yes", "no", "on" or "off"), got "<invalid>"`))
 	})
 })
 
-func ExampleGetBool() {
-	os.Setenv("FOO", "true")
-
-	v, ok, err := config.GetBool(config.Environment(), "FOO")
-	if err != nil {
-		panic(err)
-	}
-
-	if !ok {
-		fmt.Println("key is not defined!")
-	} else if v {
-		fmt.Println("true!")
-	} else {
-		fmt.Println("false!")
-	}
-
-	// Output: true!
-}
-
-var _ = Describe("func GetBoolT()", func() {
+var _ = Describe("func AsBoolF()", func() {
 	DescribeTable(
-		"it returns the expected value",
+		"it returns a boolean value",
 		func(str string, expect bool) {
 			b := Map{"<key>": String(str)}
 
-			v, err := GetBoolT(b, "<key>")
-			Expect(err).ShouldNot(HaveOccurred())
+			v := AsBoolF(b, "<key>")
 			Expect(v).To(Equal(expect))
 		},
 		boolTableEntries...,
@@ -86,44 +61,26 @@ var _ = Describe("func GetBoolT()", func() {
 	It("returns true if the key is not defined", func() {
 		b := Map{}
 
-		v, err := GetBoolT(b, "<key>")
-		Expect(err).ShouldNot(HaveOccurred())
+		v := AsBoolT(b, "<key>")
 		Expect(v).To(BeTrue())
 	})
 
-	It("returns an error if the value is not one of the accepted values", func() {
+	It("panics if the value is not one of the accepted values", func() {
 		b := Map{"<key>": String("<invalid>")}
 
-		_, err := GetBoolT(b, "<key>")
-		Expect(err).To(MatchError(`<key> is not a boolean, got "<invalid>", expected "true", "false", "yes", "no", "on" or "off"`))
+		Expect(func() {
+			AsBoolT(b, "<key>")
+		}).To(PanicWith(`expected <key> to be a boolean ("true", "false", "yes", "no", "on" or "off"), got "<invalid>"`))
 	})
 })
 
-func ExampleGetBoolT() {
-	os.Setenv("FOO", "false")
-
-	v, err := config.GetBoolT(config.Environment(), "FOO")
-	if err != nil {
-		panic(err)
-	}
-
-	if v {
-		fmt.Println("true!")
-	} else {
-		fmt.Println("false!")
-	}
-
-	// Output: false!
-}
-
-var _ = Describe("func GetBoolF()", func() {
+var _ = Describe("func AsBoolF()", func() {
 	DescribeTable(
-		"it returns the expected value",
+		"it returns a boolean value",
 		func(str string, expect bool) {
 			b := Map{"<key>": String(str)}
 
-			v, err := GetBoolF(b, "<key>")
-			Expect(err).ShouldNot(HaveOccurred())
+			v := AsBoolF(b, "<key>")
 			Expect(v).To(Equal(expect))
 		},
 		boolTableEntries...,
@@ -132,44 +89,26 @@ var _ = Describe("func GetBoolF()", func() {
 	It("returns false if the key is not defined", func() {
 		b := Map{}
 
-		v, err := GetBoolF(b, "<key>")
-		Expect(err).ShouldNot(HaveOccurred())
+		v := AsBoolF(b, "<key>")
 		Expect(v).To(BeFalse())
 	})
 
-	It("returns an error if the value is not one of the accepted values", func() {
+	It("panics if the value is not one of the accepted values", func() {
 		b := Map{"<key>": String("<invalid>")}
 
-		_, err := GetBoolF(b, "<key>")
-		Expect(err).To(MatchError(`<key> is not a boolean, got "<invalid>", expected "true", "false", "yes", "no", "on" or "off"`))
+		Expect(func() {
+			AsBoolF(b, "<key>")
+		}).To(PanicWith(`expected <key> to be a boolean ("true", "false", "yes", "no", "on" or "off"), got "<invalid>"`))
 	})
 })
 
-func ExampleGetBoolF() {
-	os.Setenv("FOO", "true")
-
-	v, err := config.GetBoolF(config.Environment(), "FOO")
-	if err != nil {
-		panic(err)
-	}
-
-	if v {
-		fmt.Println("true!")
-	} else {
-		fmt.Println("false!")
-	}
-
-	// Output: true!
-}
-
-var _ = Describe("func GetBoolDefault()", func() {
+var _ = Describe("func AsBoolDefault()", func() {
 	DescribeTable(
-		"it returns the expected value",
+		"it returns a boolean value",
 		func(str string, expect bool) {
 			b := Map{"<key>": String(str)}
 
-			v, err := GetBoolDefault(b, "<key>", true)
-			Expect(err).ShouldNot(HaveOccurred())
+			v := AsBoolDefault(b, "<key>", true)
 			Expect(v).To(Equal(expect))
 		},
 		boolTableEntries...,
@@ -178,99 +117,7 @@ var _ = Describe("func GetBoolDefault()", func() {
 	It("returns the default value if the key is not defined", func() {
 		b := Map{}
 
-		v, err := GetBoolDefault(b, "<key>", true)
-		Expect(err).ShouldNot(HaveOccurred())
-		Expect(v).To(BeTrue())
-	})
-
-	It("returns an error if the value is not one of the accepted values", func() {
-		b := Map{"<key>": String("<invalid>")}
-
-		_, err := GetBoolDefault(b, "<key>", true)
-		Expect(err).To(MatchError(`<key> is not a boolean, got "<invalid>", expected "true", "false", "yes", "no", "on" or "off"`))
-	})
-})
-
-func ExampleGetBoolDefault() {
-	os.Setenv("FOO", "")
-
-	v, err := config.GetBoolDefault(config.Environment(), "FOO", true)
-	if err != nil {
-		panic(err)
-	}
-
-	if v {
-		fmt.Println("true!")
-	} else {
-		fmt.Println("false!")
-	}
-
-	// Output: true!
-}
-
-var _ = Describe("func MustGetBool()", func() {
-	DescribeTable(
-		"it returns the expected value",
-		func(str string, expect bool) {
-			b := Map{"<key>": String(str)}
-
-			v, ok := MustGetBool(b, "<key>")
-			Expect(v).To(Equal(expect))
-			Expect(ok).To(BeTrue())
-		},
-		boolTableEntries...,
-	)
-
-	It("sets ok to false if the key is not defined", func() {
-		b := Map{}
-
-		_, ok := MustGetBool(b, "<key>")
-		Expect(ok).To(BeFalse())
-	})
-
-	It("panics if the value is not one of the accepted values", func() {
-		b := Map{"<key>": String("<invalid>")}
-
-		Expect(func() {
-			MustGetBool(b, "<key>")
-		}).To(PanicWith(
-			MatchError(`<key> is not a boolean, got "<invalid>", expected "true", "false", "yes", "no", "on" or "off"`),
-		))
-	})
-})
-
-func ExampleMustGetBool() {
-	os.Setenv("FOO", "false")
-
-	v, ok := config.MustGetBool(config.Environment(), "FOO")
-
-	if !ok {
-		fmt.Println("key is not defined!")
-	} else if v {
-		fmt.Println("true!")
-	} else {
-		fmt.Println("false!")
-	}
-
-	// Output: false!
-}
-
-var _ = Describe("func MustGetBoolT()", func() {
-	DescribeTable(
-		"it returns the expected value",
-		func(str string, expect bool) {
-			b := Map{"<key>": String(str)}
-
-			v := MustGetBoolT(b, "<key>")
-			Expect(v).To(Equal(expect))
-		},
-		boolTableEntries...,
-	)
-
-	It("returns true if the key is not defined", func() {
-		b := Map{}
-
-		v := MustGetBoolT(b, "<key>")
+		v := AsBoolDefault(b, "<key>", true)
 		Expect(v).To(BeTrue())
 	})
 
@@ -278,105 +125,7 @@ var _ = Describe("func MustGetBoolT()", func() {
 		b := Map{"<key>": String("<invalid>")}
 
 		Expect(func() {
-			MustGetBoolT(b, "<key>")
-		}).To(PanicWith(
-			MatchError(`<key> is not a boolean, got "<invalid>", expected "true", "false", "yes", "no", "on" or "off"`),
-		))
+			AsBoolDefault(b, "<key>", true)
+		}).To(PanicWith(`expected <key> to be a boolean ("true", "false", "yes", "no", "on" or "off"), got "<invalid>"`))
 	})
 })
-
-func ExampleMustGetBoolT() {
-	os.Setenv("FOO", "false")
-
-	if config.MustGetBoolT(config.Environment(), "FOO") {
-		fmt.Println("true!")
-	} else {
-		fmt.Println("false!")
-	}
-
-	// Output: false!
-}
-
-var _ = Describe("func MustGetBoolF()", func() {
-	DescribeTable(
-		"it returns the expected value",
-		func(str string, expect bool) {
-			b := Map{"<key>": String(str)}
-
-			v := MustGetBoolF(b, "<key>")
-			Expect(v).To(Equal(expect))
-		},
-		boolTableEntries...,
-	)
-
-	It("returns false if the key is not defined", func() {
-		b := Map{}
-
-		v := MustGetBoolF(b, "<key>")
-		Expect(v).To(BeFalse())
-	})
-
-	It("panics if the value is not one of the accepted values", func() {
-		b := Map{"<key>": String("<invalid>")}
-
-		Expect(func() {
-			MustGetBoolF(b, "<key>")
-		}).To(PanicWith(
-			MatchError(`<key> is not a boolean, got "<invalid>", expected "true", "false", "yes", "no", "on" or "off"`),
-		))
-	})
-})
-
-func ExampleMustGetBoolF() {
-	os.Setenv("FOO", "true")
-
-	if config.MustGetBoolF(config.Environment(), "FOO") {
-		fmt.Println("true!")
-	} else {
-		fmt.Println("false!")
-	}
-
-	// Output: true!
-}
-
-var _ = Describe("func MustGetBoolDefault()", func() {
-	DescribeTable(
-		"it returns the expected value",
-		func(str string, expect bool) {
-			b := Map{"<key>": String(str)}
-
-			v := MustGetBoolDefault(b, "<key>", true)
-			Expect(v).To(Equal(expect))
-		},
-		boolTableEntries...,
-	)
-
-	It("returns the default value if the key is not defined", func() {
-		b := Map{}
-
-		v := MustGetBoolDefault(b, "<key>", true)
-		Expect(v).To(BeTrue())
-	})
-
-	It("panics if the value is not one of the accepted values", func() {
-		b := Map{"<key>": String("<invalid>")}
-
-		Expect(func() {
-			MustGetBoolDefault(b, "<key>", true)
-		}).To(PanicWith(
-			MatchError(`<key> is not a boolean, got "<invalid>", expected "true", "false", "yes", "no", "on" or "off"`),
-		))
-	})
-})
-
-func ExampleMustGetBoolDefault() {
-	os.Setenv("FOO", "")
-
-	if config.MustGetBoolDefault(config.Environment(), "FOO", true) {
-		fmt.Println("true!")
-	} else {
-		fmt.Println("false!")
-	}
-
-	// Output: true!
-}
