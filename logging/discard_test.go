19bd8f7f6f17023215a6674b4cc958aad93af640
James Harris <contact@jamesharris.id.au>
2022-01-11 10:51:20 +1000

    Cover no-op methods of `DiscardLogger`.

--- logging/discard_test.go
+++ logging/discard_test.go
@@ -7,9 +7,35 @@ import (
 )
 
 var _ = Describe("type DiscardLogger", func() {
+	var logger DiscardLogger
+
+	Describe("func Log()", func() {
+		It("does not panic", func() {
+			logger.Log("<message>")
+		})
+	})
+
+	Describe("func LogString()", func() {
+		It("does not panic", func() {
+			logger.LogString("<message>")
+		})
+	})
+
+	Describe("func Debug()", func() {
+		It("does not panic", func() {
+			logger.Debug("<message>")
+		})
+	})
+
+	Describe("func DebugString()", func() {
+		It("does not panic", func() {
+			logger.DebugString("<message>")
+		})
+	})
+
 	Describe("func IsDebug()", func() {
 		It("returns false", func() {
-			Expect(DiscardLogger{}.IsDebug()).To(BeFalse())
+			Expect(logger.IsDebug()).To(BeFalse())
 		})
 	})
 })
